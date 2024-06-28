99715fcea3ed9950070f8e1ef50b1dd459ecd446
Rajanith Bandara <RajanithBandara123@outlook.com>
2024-06-28 14:16:23 +0530

    some graphical changes done

---
+++ index.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const darkMode = document.getElementById('dark-mode');
+    const lightMode = document.getElementById('light-mode');
+
+    function setTheme(theme){
+        if (theme == "dark"){
+            lightMode.disabled = true;
+            darkMode.disabled = false;
+        } else{
+            lightMode.disabled = false;
+            darkMode.disabled = true;
+        }
+    }
+
+    const PrefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+    setTheme(PrefersDarkScheme.matches ? "dark" : "light");
+
+    window.matchMedia("(prefers-color-scheme: dark)").addEventListener('change',event => {
+        setTheme(event.matches ? "dark" : "light");
+    });
+
+    const themeswitch = document.getElementById('themeSwitch');
+      themeswitch.onchange = () => {
+        if (themeswitch.checked) {
+          document.getElementById('light-mode').disabled = true;
+          document.getElementById('dark-mode').disabled = false;
+          document.getElementById('themeIcon').classList.remove('fa-sun');
+          document.getElementById('themeIcon').classList.add('fa-moon');
+        } else {
+          document.getElementById('light-mode').disabled = false;
+          document.getElementById('dark-mode').disabled = true;
+          document.getElementById('themeIcon').classList.remove('fa-moon');
+          document.getElementById('themeIcon').classList.add('fa-sun');
+        }
+      };
+});
