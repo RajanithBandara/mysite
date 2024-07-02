bfdbeca5ff7376b84575d191470d2ca31fd82dfd
Rajanith Bandara <RajanithBandara123@outlook.com>
2024-07-02 12:04:47 +0530

    ss

--- footer/footer.php
+++ footer/footer.php
@@ -2,9 +2,9 @@
         <div class="footer-content">
             <div class="footer-info">
                 <h5>Connect with me</h5>
-                <a href="https://www.linkedin.com" class="footer-link"><i class="fab fa-linkedin"></i> LinkedIn</a>
-                <a href="https://github.com" class="footer-link"><i class="fab fa-github"></i> GitHub</a>
-                <a href="https://twitter.com" class="footer-link"><i class="fab fa-twitter"></i> Twitter</a>
+                <a href="https://www.linkedin.com/in/rajanith-bandara-373920264/" class="footer-link"><i class="fab fa-linkedin"></i> LinkedIn</a>
+                <a href="https://github.com/RajanithBandaracom" class="footer-link"><i class="fab fa-github"></i> GitHub</a>
+                <a href="https://x.com/BandaraRajanith" class="footer-link"><i class="fab fa-twitter"></i> Twitter</a>
             </div>
             <div class="footer-info">
                 <h5>Quick Links</h5>
